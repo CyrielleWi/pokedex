48b4886abefa30c07d8bd85b3395477c8f49ab50
Cyrielle Willerval <cyrielle.willerval@ensta-paristech.fr>
2019-04-04 12:32:17 +0200

    add Drawer when avatar is clicked

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '@material-ui/core/Avatar';
+import Drawer from '@material-ui/core/Drawer';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
     div: {
         width: '250px',
-        borderColor: 'black',
-        borderStyle: 'double',
-        borderWidth: '6px',
         marginLeft: 'auto',
         margiRight: 'auto',
         textAlign: 'center',
@@ -17,6 +15,10 @@ const styles = {
         paddingBottom: '5px',
         fontFamily: "Pokemon GB", 
         fontSize: '10px',
+    },
+    name: {
+      fontSize: '15px',
+      marginBottom: '-3px',
     },
     avatar: {
       margin: 10,
@@ -27,7 +29,17 @@ const styles = {
 
 class Pokemon extends React.Component {
 
-    render() {
+  state = {
+    isOpen: false,
+  }
+
+  toggleDrawer = (open) => () => {
+    this.setState({
+      isOpen: open,
+    });
+  };
+
+  render() {
 
     const { pokemonInfo, classes } = this.props;
 
@@ -37,7 +49,22 @@ class Pokemon extends React.Component {
 
     let name = pokemonInfo.name[0].toUpperCase() + pokemonInfo.name.slice(1);
 
-    return(<Avatar alt={name} src={pokemonInfo.image} className={classes.avatar}/>);
+    return(<div><Avatar alt={name} src={pokemonInfo.image} className={classes.avatar} onClick={this.toggleDrawer(true)}/>
+      <Drawer open={this.state.isOpen} onClose={this.toggleDrawer(false)}>
+    <div
+      tabIndex={0}
+      role="button"
+      onClick={this.toggleDrawer(false)}
+      onKeyDown={this.toggleDrawer(false)}
+    >
+      {(<div className={classes.div}>
+      <p className={classes.name}>{name}</p>
+      <img src={pokemonInfo.image} alt={pokemonInfo.name} /> <br />
+      Attack : {pokemonInfo.ability}<br />
+      Weight : {pokemonInfo.weight} kg
+      </div>)}
+    </div>
+  </Drawer></div>);
     }
 }
 
